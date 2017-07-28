586b0068c95b9f3aeccd67f4aaeb179d4a0d1a5b
root <florian.baer@baerdev.com>
2017-07-28 16:36:20 +0200

    create usage

--- NetCat/netcat_clone.py
+++ NetCat/netcat_clone.py
@@ -12,3 +12,25 @@ target              =   ""
 upload_destination  =   ""
 port                =   3344
 
+def usage():
+    print "BHP Net Tool"
+    print
+    print "Usage bhpnet.py -t target_host -p port"
+    print "-l --listen              - listen at [host]:[port] for incoming connections"
+    print "-e --ececute=file_to_run - execute the given file upon a incoming connection"
+    print "-c --command             - initialize a command shell"
+    print "-u --upload=destination  - upon receiving a connection upload a file to [destination]"
+    print 
+    print
+    print "Example usages:"
+    print "bhpnet.py -t 192.168.0.1 -p 5555 -l -c"
+    print "bhpnet.py -t 192.168.0.1 -p 5555 -l -u=c\\target.exe"
+    print "bhpnet.py -t 192.168.0.1 -p 5555 -l -e\"cat /etc/passwd\""
+    print "'echo ABCDEFGHI' | ./bhpnet.py -t 192.168.1.12 -p 333"
+    sys.exit(0)
+
+def main():
+    global listen 
+
+
+usage()
